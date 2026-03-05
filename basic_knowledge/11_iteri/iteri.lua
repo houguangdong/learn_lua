f0286cbf56ec1eba1735f0562f161c28ed7a157a
houguangdong <houguangdong@jxqygames.com>
2026-03-05 20:52:41 +0800

    table

--- basic_knowledge/11_iteri/iteri.lua
+++ basic_knowledge/11_iteri/iteri.lua
@@ -2,14 +2,12 @@
 
 --Lua 迭代器
 --迭代器（iterator）是一种对象，它能够用来遍历标准模板库容器中的部分或全部元素，每个迭代器对象代表容器中的确定的地址。
---
 --在 Lua 中迭代器是一种支持指针类型的结构，它可以遍历集合的每一个元素。
 --
 --泛型 for 迭代器
 --泛型 for 在自己内部保存迭代函数，实际上它保存三个值：迭代函数、状态常量、控制变量。
 --
 --泛型 for 迭代器提供了集合的 key/value 对，语法格式如下：
---
 --for k, v in pairs(t) do
 --    print(k, v)
 --end
@@ -24,9 +22,7 @@ end
 print('----------------------')
 
 --以上实例中我们使用了 Lua 默认提供的迭代函数 ipairs。
---
 --下面我们看看泛型 for 的执行过程：
---
 --首先，初始化，计算 in 后面表达式的值，表达式应该返回泛型 for 需要的三个值：迭代函数、状态常量、控制变量；与多值赋值一样，如果表达式返回的结果个数不足三个会自动用 nil 补足，多出部分会被忽略。
 --第二，将状态常量和控制变量作为参数调用迭代函数（注意：对于 for 结构来说，状态常量没有用处，仅仅在初始化时获取他的值并传递给迭代函数）。
 --第三，将迭代函数返回的值赋给变量列表。
@@ -94,9 +90,7 @@ for element in elementIterator(array)
 do
    print(element)
 end
-
 print('----------------------')
-
 --pairs 和 ipairs区别
 --pairs: 迭代 table，可以遍历表中所有的 key 可以返回 nil
 --ipairs: 迭代数组，不能返回 nil,如果遇到 nil 则退出
@@ -105,27 +99,23 @@ for i, v in pairs(tab) do
     print(tab[i])
 end
 
-for i, v in ipairs(tab) do -- 输出 "a" ,k=2时断开  
+for i, v in ipairs(tab) do -- 输出 "a", k=2时断开
     print(tab[i])
 end
-
 print('----------------------')
 --pairs 和 ipairs异同
 --同：都是能遍历集合（表、数组）
 --异：ipairs 仅仅遍历值，按照索引升序遍历，索引中断停止遍历。即不能返回 nil,只能返回数字 0，如果遇到 nil 则退出。它只能遍历到集合中出现的第一个不是整数的 key。
 --pairs 能遍历集合的所有元素。即 pairs 可以遍历集合中所有的 key，并且除了迭代器本身以及遍历表本身还可以返回 nil
 local tabFiles = {
-        [1] = "test2",
-        [6] = "test3",
-        [4] = "test1"
-    }
+    [1] = "test2",
+    [6] = "test3",
+    [4] = "test1"
+}
 
 for k, v in ipairs(tabFiles) do    --输出"test2",在key等于2处断开
     print(k, v)
 end
-
-
-
 print('----------------------')
 local tabFiles = {
     [2] = "test2",
@@ -136,7 +126,6 @@ local tabFiles = {
 for k, v in ipairs(tabFiles) do  --[[什么都没输出，为什么？因为控制变量初始值是按升序来遍历的，当key为1时，value为nil，此时便停止了遍历， 所有什么结果都没输出]]--
     print(k, v)
 end
-
 print('----------------------')
 local tabFiles = {
     [2] = "test2",
@@ -147,13 +136,8 @@ local tabFiles = {
 for k, v in pairs(tabFiles) do  --输出2 test2, 6 test3, 4 test1
     print(k, v)
 end
-
 print('----------------------')
-
-
-
-
-local tabFiles = {"alpha", "beta", [3] = "no", ["two"] = "yes"}  for i,v in ipairs(tabFiles ) do    --输出前三个   备注：因为第四个key不是整数
+local tabFiles = {"alpha", "beta", [3] = "no", ["two"] = "yes"}  for i, v in ipairs(tabFiles ) do    --输出前三个   备注：因为第四个key不是整数
     print( tabFiles [i] )   
 end   
   
@@ -161,7 +145,7 @@ for i,v in pairs(tabFiles ) do    --全部输出
     print( tabFiles [i] )   
 end
 print('----------------------')
-function split(str,delimiter)
+function split(str, delimiter)
     local dLen = string.len(delimiter)
     local newDeli = ''
     for i=1,dLen,1 do
--- basic_knowledge/20_database/db.lua
+++ basic_knowledge/20_database/db.lua
@@ -4,21 +4,25 @@
 --本文为大家介绍MySQL的数据库连接。
 --LuaSQL 可以使用 LuaRocks 来安装可以根据需要安装你需要的数据库驱动。
 --LuaRocks 安装方法：
+
 --$ wget http://luarocks.org/releases/luarocks-2.2.1.tar.gz
 --$ tar zxpf luarocks-2.2.1.tar.gz
 --$ cd luarocks-2.2.1
 --$ ./configure; sudo make bootstrap
 --$ sudo luarocks install luasocket
+
 --$ lua
 --Lua 5.3.0 Copyright (C) 1994-2015 Lua.org, PUC-Rio
 --> require "socket"
 --Window 下安装 LuaRocks：https://github.com/keplerproject/luarocks/wiki/Installation-instructions-for-Windows
+
 --安装不同数据库驱动：
 --luarocks install luasql-sqlite3
 --luarocks install luasql-postgres
 --luarocks install luasql-mysql
 --luarocks install luasql-sqlite
 --luarocks install luasql-odbc
+
 --你也可以使用源码安装方式，Lua Github 源码地址：https://github.com/keplerproject/luasql
 --Lua 连接MySql 数据库：
 require "luasql.mysql"
@@ -39,22 +43,17 @@ row = cur:fetch({},"a")
 
 --文件对象的创建
 file = io.open("role.txt","w+");
-
 while row do
     var = string.format("%d %s\n", row.id, row.name)
-
     print(var)
-
     file:write(var)
-
     row = cur:fetch(row,"a")
 end
-
 
 file:close()  --关闭文件对象
 conn:close()  --关闭数据库连接
 env:close()   --关闭数据库环境
-
+print('------------------------------------')
 --5.2 版本之后，require 不再定义全局变量，需要保存其返回值。
 require "luasql.mysql"
 --需要写成:
